d7d4bc86e30b5e700433ed84bc4897a41eca33a8
Mark Edison Jayag <macvillaflores000@gmail.com>
2023-11-21 21:32:49 +0800

    Authentication Templates

--- document_manager/urls.py
+++ document_manager/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('home', views.home, name='home')
 ]
