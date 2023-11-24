d889d40884135fdbbf5e1b6d14ceee1345bf696f
PSU-Maica <202280193@psu.palawan.edu.ph>
2023-11-25 01:16:17 +0800

    added CustomUser model (for user_type field)

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,23 @@
 from django import forms
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
-
+    
 
 class RegistrationForm(UserCreationForm):
+    user_type_choices = [
+        ('student', 'Student'),
+        ('faculty', 'Faculty'),
+        ('admin', 'Admin'),
+        ('alumni', 'Alumni')
+    ]
+    user_type = forms.ChoiceField(choices=user_type_choices, widget=forms.Select)
+
+
     class Meta:
-        model=User
-        fields = ['username', 'email', 'password1', 'password2']
+        model=CustomUser
+        fields = ['username', 'email', 'user_type', 'password1', 'password2']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    user_type_choices = [
+        ('student', 'Student'),
+        ('faculty', 'Faculty'),
+        ('admin', 'Admin'),
+        ('alumni', 'Alumni')
+    ]
+    user_type = models.CharField(max_length=20, choices=user_type_choices, default='student')
+
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True, verbose_name=_('groups'))
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True, verbose_name=_('user permissions'))
